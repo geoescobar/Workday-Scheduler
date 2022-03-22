19746e85e2d24b74e931d6952636b8d3c39e2aad
George Escobar <geo.escobar214@gmail.com>
2022-03-21 20:51:29 -0500

    update

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,10 @@
-// current day variable in the header
+// variable to add current date in the header to update daily 
 var today = moment();
 $("#currentDay").text(today.format("dddd, MMMM Do"));
+
+// create var for the scheduled workday hours 
+var scheduledHours = [];
+
+// create a for loop for the hours of 9am-5pm
+for (var hour = 9; hour < 18; hour++);
 
