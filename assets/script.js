3baf3f5cf9224a37c6adf1bc76d06ae124c7918d
George Escobar <geo.escobar214@gmail.com>
2022-03-22 06:53:55 -0500

    update

--- assets/script.js
+++ assets/script.js
@@ -5,6 +5,25 @@ $("#currentDay").text(today.format("dddd, MMMM Do"));
 // create var for the scheduled workday hours 
 var scheduledHours = [];
 
-// create a for loop for the hours of 9am-5pm
-for (var hour = 9; hour < 18; hour++);
-
+// create a for loop for the hours of 9am-5pm & design 
+// center save button 
+for (var hour = 9; hour < 18; hour++) {
+    scheduledHours.push(moment({
+        hour
+    }).format('h  a'));
+    $('.container').append(`<div class="row time-block" data-time="${hour}"> 
+       <!--hour column-->
+           <div class="col-sm col-md-2 hour"> 
+             <p class=dayHour>${moment({hour}).format('h  a')}</p>
+           </div> 
+           
+       <!-- input text area-->
+           <div class="col-sm col-md-8 d-flex description"> 
+             <textarea class=textArea></textarea> 
+           </div> 
+      
+       <!--save button-->
+           <div class="col-sm col-md-2 saveBtn">
+           <i class="far fa-save fa-2x" id=icon></i>  
+           </div>`);
+}
